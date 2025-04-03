ec2b110fe25dccc43bd72cf1bf757abeb38b4b43
ChristianCamiloP <149966264+ChristianCamiloP@users.noreply.github.com>
2025-04-03 02:44:35 +0000

    cambio_2

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,37 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
+
 import Listar from './componentes/listar'
 import Original from './componentes/original'
 import Aleatorios from './componentes/aleatorios'
 import Detalle from './componentes/detalle'
 import Usuario from './componentes/usuario'
 import Favoritos from './componentes/favoritos'
-
+import Menu from './componentes/menu'
 
 
 function App() {
 
   return (
-    <>
-  <Listar/>
-  <Original/>
-  <Aleatorios/>
-  <Detalle/>
-  <Usuario/>
-  <Favoritos/>
-  
-    </>
+    <Router>
+
+    <Menu />
+   
+    <Routes>
+
+        <Route path="/" element={<Listar />} />
+        <Route path="/usuarios" element={<Usuario />} />
+        <Route path="/aleatorios" element={<Aleatorios />} />
+        <Route path="/original" element={<Original />} />
+        <Route path="/favoritos" element={<Favoritos />} />
+        <Route path="/:nombre" element={<Detalle />} />
+
+    </Routes>
+ 
+  </Router>
+
+
   )
 }
 
--- src/componentes/menu/index.jsx
+++ src/componentes/menu/index.jsx
@@ -1,32 +1,17 @@
-import { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import "./style.css"
+import { Link } from 'react-router-dom';
 
-import Menu from './componentes/menu'
-import Listar from './componentes/listar'
-import Original from './componentes/original'
-import Aleatorios from './componentes/aleatorios'
-import Detalle from './componentes/detalle'
-import Usuario from './componentes/usuario'
-import Favoritos from './componentes/favoritos'
-
-function App() {
-
-  return (
-    <Router>
-
-      <Menu />
-     
-      <Routes>
-        <Route path="/" element={<Lista />} />
-        <Route path="/usuarios" element={<Usuarios />} />
-        <Route path="/aleatorios" element={<Aleatorios />} />
-        <Route path="/capturados" element={<Capturados />} />
-        <Route path="/favoritos" element={<Favoritos />} />
-        <Route path="/:nombre" element={<Detalle />} />
-      </Routes>
-   
-    </Router>
-  );
-}
-
-export default App;
+function Menu() {
+    return (
+        <nav className="c-menu">
+          <Link to="/">Lista</Link>
+          <Link to="/detalle">Detalle</Link>
+          <Link to="/aleatorios">Aleatorio</Link>
+          <Link to="/usuarios">Usuarios</Link>
+          <Link to="/favoritos">Favoritos</Link>
+        </nav>
+    )
+  }
+ 
+  export default Menu
+  
